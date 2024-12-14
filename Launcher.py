8ddf841e2b54b0343838f2b69cbe3940ce1367aa
Martin Schumacher <msc@keineantwort.de>
2024-12-14 12:59:25 +0100

    introducing working_dir

--- Launcher.py
+++ Launcher.py
@@ -8,20 +8,21 @@ from pla import PaperlessAccess
 
 
 class Launcher:
-    def __init__(self, document_id: int, config_file="launcher.yaml"):
-        with open(config_file, 'r') as f:
+    def __init__(self, document_id: int, working_dir=os.getcwd()):
+        with open(os.path.join(working_dir, "launcher.yaml"), 'r') as f:
             self._config = yaml.safe_load(f)
-            self._rootdir = os.getcwd()
+            self._scriptdir = os.getcwd()
+            self._workingdir = working_dir
             self._document_id = document_id
 
     def launch_post_consumption(self):
         paperless_access = PaperlessAccess(url=self._config["credentials"]["url"], token=self._config["credentials"]["api_token"])
         for module in self._config["postconsumption"]:
             print(f"launching {module}")
-            path = os.path.join(self._rootdir, module)
-            config_file = os.path.join(self._rootdir, f"{module}.yaml")
+            path = os.path.join(self._scriptdir, module)
+            config_file = os.path.join(self._workingdir, f"{module}.yaml")
             script = importlib.import_module(f".main", package=module)
             os.chdir(path)
             print(f"set working dir to {path}")
             asyncio.run(script.main(paperless_access, self._document_id, config_file))
-            os.chdir(self._rootdir)
+            os.chdir(self._scriptdir)
--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from Launcher import Launcher
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Post consumption script for paperless ngx.')
     parser.add_argument('-d', '--document_id', type=int, help='the ID for the document to handle')
-    parser.add_argument('-f', '--config_file', type=str, help='the configuration file for this script.', default="launcher.yaml")
+    parser.add_argument('-w', '--working_dir', type=str, help='the owrking dir for this script.', default=os.getcwd())
     args = parser.parse_args()
 
-    print(f"Launching post consumption for {args.document_id} with configuration in {args.config_file}.")
+    print(f"Launching post consumption for {args.document_id} with configuration in {args.working_dir}.")
 
-    launcher = Launcher(document_id=args.document_id, config_file=args.config_file)
+    launcher = Launcher(document_id=args.document_id, working_dir=args.working_dir)
     launcher.launch_post_consumption()
